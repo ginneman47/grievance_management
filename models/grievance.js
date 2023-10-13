c3f2edea6fa90470245a60bbdd690a7255ca4b05
Joseph <105716769+ginneman47@users.noreply.github.com>
2023-10-13 22:12:53 +0530

    Add files via upload

---
+++ models/grievance.js
@@ -0,0 +1,19 @@
+// const mongoose = require('mongoose');
+//
+// const grievanceSchema = new mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true
+//   },
+//   description: {
+//     type: String,
+//     required: true
+//   },
+//   createdBy: {
+//     type: String,
+//     required: true
+//   }
+// });
+//
+// module.exports = mongoose.model('Grievance', grievanceSchema);
+//mongoose is not working so this file is no longer needed
---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+//not needed for now everthing is done in app.js file. the data is captured using the bodyParser
